783187dab17e23f5df9c1e8ac403f44492908a2f
surendra kumar <surendrakeerbaniya@gmail.com>
2024-06-25 22:49:45 +0530

    routes

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,16 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Express' });
 });
 
+router.get('/library', function(req, res, next) {
+  res.render('library', { title: 'Express' });
+});
+
 router.get('/feedback', function(req, res, next) {
   res.render('feedback', { title: 'Feedback' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
 });
 
 module.exports = router;
